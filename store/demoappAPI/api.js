89002bdd61f978514d822e2dd18ea8b726421696
Crowdbotics <engineering@crowdbotics.com>
2023-07-06 13:53:11 +0000

    Update store.

--- store/demoappAPI/api.js
+++ store/demoappAPI/api.js
@@ -1,96 +1,68 @@
-import axios from "axios";
+import axios from "axios"
 const demoappAPI = axios.create({
   baseURL: "https://demo-app-40357.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_v1_test_list(payload) {
-  return demoappAPI.get(`/api/v1/test/`);
+  return demoappAPI.get(`/api/v1/test/`)
 }
-
 function api_v1_test_create(payload) {
-  return demoappAPI.post(`/api/v1/test/`, payload.data);
+  return demoappAPI.post(`/api/v1/test/`, payload.data)
 }
-
 function api_v1_login_create(payload) {
-  return demoappAPI.post(`/api/v1/login/`, payload.data);
+  return demoappAPI.post(`/api/v1/login/`, payload.data)
 }
-
 function api_v1_signup_create(payload) {
-  return demoappAPI.post(`/api/v1/signup/`, payload.data);
+  return demoappAPI.post(`/api/v1/signup/`, payload.data)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return demoappAPI.get(`/rest-auth/user/`);
+  return demoappAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return demoappAPI.put(`/rest-auth/user/`, payload.data);
+  return demoappAPI.put(`/rest-auth/user/`, payload.data)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return demoappAPI.patch(`/rest-auth/user/`, payload.data);
+  return demoappAPI.patch(`/rest-auth/user/`, payload.data)
 }
-
 function api_docs_schema_retrieve(payload) {
-  return demoappAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+  return demoappAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
 }
-
 function rest_auth_login_create(payload) {
-  return demoappAPI.post(`/rest-auth/login/`, payload.data);
+  return demoappAPI.post(`/rest-auth/login/`, payload.data)
 }
-
 function api_v1_test_retrieve(payload) {
-  return demoappAPI.get(`/api/v1/test/${payload.id}/`);
+  return demoappAPI.get(`/api/v1/test/${payload.id}/`)
 }
-
 function api_v1_test_update(payload) {
-  return demoappAPI.put(`/api/v1/test/${payload.id}/`, payload.data);
+  return demoappAPI.put(`/api/v1/test/${payload.id}/`, payload.data)
 }
-
 function api_v1_test_partial_update(payload) {
-  return demoappAPI.patch(`/api/v1/test/${payload.id}/`, payload.data);
+  return demoappAPI.patch(`/api/v1/test/${payload.id}/`, payload.data)
 }
-
 function api_v1_test_destroy(payload) {
-  return demoappAPI.delete(`/api/v1/test/${payload.id}/`);
+  return demoappAPI.delete(`/api/v1/test/${payload.id}/`)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return demoappAPI.get(`/rest-auth/logout/`);
+  return demoappAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return demoappAPI.post(`/rest-auth/logout/`);
+  return demoappAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_registration_create(payload) {
-  return demoappAPI.post(`/rest-auth/registration/`, payload.data);
+  return demoappAPI.post(`/rest-auth/registration/`, payload.data)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return demoappAPI.post(`/rest-auth/password/reset/`, payload.data);
+  return demoappAPI.post(`/rest-auth/password/reset/`, payload.data)
 }
-
 function rest_auth_password_change_create(payload) {
-  return demoappAPI.post(`/rest-auth/password/change/`, payload.data);
+  return demoappAPI.post(`/rest-auth/password/change/`, payload.data)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return demoappAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
+  return demoappAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return demoappAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
+  return demoappAPI.post(`/rest-auth/registration/verify-email/`, payload.data)
 }
-
 export const apiService = {
   api_v1_test_list,
   api_v1_test_create,
@@ -112,4 +84,4 @@ export const apiService = {
   rest_auth_password_change_create,
   rest_auth_password_reset_confirm_create,
   rest_auth_registration_verify_email_create
-};
+}
